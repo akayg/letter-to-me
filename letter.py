e982d85aed4921b7c1975970b3804fe8e81967ca
ABHISHEK GORE <94378927+akayg@users.noreply.github.com>
2025-01-07 12:59:51 +0530

    commit

---
+++ letter.py
@@ -0,0 +1,10 @@
+#this is a letter to future me.
+letter= """Dear ME,
+I hope you are still doing good? its just a self mail to myself to remind me from where i started and when i started,
+I have a long way to chase and alot to achive. this is a part of it .if you are reading this , is that mean you did achived something? 
+right na? if yes then add a file letter-2 in this repo to yourself telling how you did it.
+an if not then no worries , just keep going.
+Yourself
+date 07/01/2025
+All the best"""
+print(letter)
